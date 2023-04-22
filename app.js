a310c63d185c19d48e68401b91e1bd6af029b1bf
darkyHC <108454489+darkyHC@users.noreply.github.com>
2023-04-23 01:18:42 +0200

    deleting vercel and updating app.js

--- app.js
+++ app.js
@@ -23,28 +23,6 @@ app.use(passport.initialize());
 // Import and configure passport
 require("./config/passport")(passport);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token,Origin, X-Requested-With, Content, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
-
-app.use((req, res) => {
-  res.json({ message: "Working !" }); 
-});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);

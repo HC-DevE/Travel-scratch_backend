3477dc8a8ad1712aedf4a2942beb1204a73c7c8a
darkyHC <108454489+darkyHC@users.noreply.github.com>
2023-05-12 00:14:11 +0200

    update mediaservice upload file

--- services/mediaService.js
+++ services/mediaService.js
@@ -1,5 +1,6 @@
 const sequelize = require("../config/db");
 const Media = require("../models/Media")(sequelize);
+const crypto = require("crypto");
 const AWS = require("aws-sdk");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -42,18 +43,27 @@ const s3 = new AWS.S3({
 async function uploadMedia(fileBuffer, mimeType) {
   const fileExtension = mimeType.split("/")[1];
   const folderName = mimeType.startsWith("image/") ? "images" : "videos";
-  // const fileName = `${folderName}/${Date.now()}.${fileExtension}`;
-  const fileName = `uploads/${folderName}/${Date.now()}.${fileExtension}`;
+
+  const timestamp = Date.now(); //change this to a better date
+  const fileHash = crypto.createHash('md5').update(fileBuffer).digest('hex');
+  // console.log(fileHash);
+  const fileName = `uploads/${folderName}/${fileHash}_${timestamp}.${fileExtension}`;
   const params = {
     Bucket: process.env.DO_SPACES_BUCKET,
     Key: fileName,
     Body: fileBuffer,
     ACL: "public-read",
     ContentType: mimeType,
+    CacheControl: "max-age=3600", // Set cache-control to enable caching for 1 hour
   };
-  await s3.upload(params).promise();
-  const url = `https://${process.env.DO_SPACES_BUCKET}.${process.env.DO_SPACES_ENDPOINT}/${fileName}`;
-  return { url };
+  try {
+    await s3.upload(params).promise();
+    const url = `https://${process.env.DO_SPACES_BUCKET}.${process.env.DO_SPACES_ENDPOINT}/${fileName}`;
+    return { url };
+  } catch (error) {
+    console.error("Failed to upload media:", error);
+    throw error;
+  }
 }
 
 async function saveMediaToDatabase(
@@ -71,7 +81,6 @@ async function saveMediaToDatabase(
       type: mediaType,
       url: mediaUrl,
     });
-    console.log("the media from mediaService", media);
     return media.id;
   } catch (error) {
     console.error(error);
